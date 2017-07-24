0c928e6a985faf7c2936ff0fca43fadf4590ea8d
cread <charlesread@clayton.edu>
2017-07-24 13:20:32 -0400

    another example

---
+++ example/2 - withHapiAclAuth.js
@@ -0,0 +1,101 @@
+'use strict'
+
+const path = require('path')
+const Hapi = require('hapi')
+
+const plugin = require(path.join(__dirname, '..', 'index.js'))
+
+const db = {
+  'cread': {
+    name: 'Charles Read',
+    password: 'cricket',
+    roles: ['SUPERUSER']
+  },
+  'lread': {
+    name: 'Ladybug',
+    password: 'treats',
+    roles: ['ADMIN']
+  },
+  'jsmith1': {
+    name: 'John Smith',
+    password: 'passw0rd',
+    roles: ['USER']
+  }
+}
+
+const plugins = [
+  {
+    register: plugin,
+    options: {
+      handler: function (username, password, callback) {
+        const user = db[username]
+        if (!user) {
+          return callback(false)
+        }
+        if (user.password === password) {
+          return callback(true, {username, name: user.name, roles: user.roles})
+        } else {
+          return callback(false)
+        }
+      }
+    }
+  },
+  {
+    register: require('hapi-acl-auth'),
+    options: {
+      handler: function (request, callback) {
+        callback(null, request.auth.credentials)
+      },
+      hierarchy: ['USER', 'ADMIN', 'SUPERUSER'],
+      policy: 'allow'
+    }
+  }
+]
+
+const server = new Hapi.Server()
+
+server.connection({
+  host: 'localhost',
+  port: 8000
+})
+
+server.register(plugins, function (err) {
+  if (err) {
+    throw err
+  }
+  // the first argument can really be anything, it's just an identifier that
+  // is to be used in a route's config.auth attribute, as shown below
+  server.auth.strategy('hfa', 'form')
+  // an insecure route
+  server.route({
+    method: 'get',
+    path: '/',
+    handler: function (request, reply) {
+      return reply('/')
+    }
+  })
+  // a secure route
+  server.route({
+    method: 'get',
+    path: '/secure',
+    handler: function (request, reply) {
+      return reply('secure, username: ' + request.auth.credentials.username)
+    },
+    config: {
+      auth: 'hfa',
+      plugins: {
+        hapiAclAuth: {
+          roles: ['ADMIN'],
+          secure: true
+        }
+      }
+    }
+  })
+})
+
+server.start((err) => {
+  if (err) {
+    throw err
+  }
+  console.log('Server running at:', server.info.uri)
+})
